6bea6a8cdbea3cb3b58914a73dc1c12e43ca6455
Sushanth-S-Rao <sushanthsraotth@gmail.com>
2023-02-13 01:36:49 +0530

    Added template and view for detailed post as well as CSS

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name = 'post_list'),
+    path('post/<int:pk>/', views.post_detail, name = 'post_detail'),
 ]
 
 # matches empty string (ignoring domain name, https:\\sushanthrao.pythonanywhere.com\) and redirects it to view path_list
